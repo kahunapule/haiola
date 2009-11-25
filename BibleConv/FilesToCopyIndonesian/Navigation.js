3ed5c5606400bd9764387bdb67615fb60c6a808d
Michael Johnson <Kahunapule_Johnson@sil.org>
2009-11-26 08:05:44 +1000

    Cosmetic changes

---
+++ BibleConv/FilesToCopyIndonesian/Navigation.js
@@ -0,0 +1,17 @@
+﻿// JavaScript functions to highlight text in main window of concordance.
+
+function SetBookName(bookName)
+{
+	var target = document.getElementById("book");
+	var replacement = document.createTextNode(bookName);
+	for (var i = 0; i < target.childNodes.length; i++)
+	{
+		if (target.childNodes[i].nodeType == 3)
+		{
+			target.replaceChild(replacement, target.childNodes[i])
+			return 0;
+		}
+	}
+	// no suitable child nodes to replace.
+	target.appendChild(replacement);
+}
