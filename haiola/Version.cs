825e9de7e59e6052c2bddcfb5080a277d99cff30
Kahunapule <Kahunapule@localhost>
2012-04-12 12:34:20 -1000

    Test of release script.

--- haiola/Version.cs
+++ haiola/Version.cs
@@ -8,7 +8,7 @@ namespace haiola
 	{
 		public static string date = "2012-04-12";
 		public static string year = @"2012";
-		public static string time = @"03:29:24";
+		public static string time = @"03:29:25";
 		public Version()
 		{
 		}
