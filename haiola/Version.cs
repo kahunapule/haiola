e2fcd9286460ee2e220454afc2ff3e5bc5a19771
Kahunapule <Kahunapule@localhost>
2012-04-11 20:26:13 -1000

    Made it so that we don't choke on \periph tags.

--- haiola/Version.cs
+++ haiola/Version.cs
@@ -6,9 +6,9 @@ namespace haiola
 	/// </summary>
 	public class Version
 	{
-		public static string date = "2012-04-05";
+		public static string date = "2012-04-12";
 		public static string year = @"2012";
-		public static string time = @"06:59:50";
+		public static string time = @"03:29:24";
 		public Version()
 		{
 		}
