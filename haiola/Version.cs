5d5152b9ded1975261bf6a37997c355f049e0563
Kahunapule <Kahunapule@localhost>
2012-11-02 10:37:04 -1000

    Quick bug fix: preserve "nonsignificant" XML white space on USFX import, because some of that space really is significant.

--- haiola/Version.cs
+++ haiola/Version.cs
@@ -6,9 +6,9 @@ namespace haiola
 	/// </summary>
 	public class Version
 	{
-		public static string date = "2012-11-01";
+		public static string date = "2012-11-02";
 		public static string year = @"2012";
-		public static string time = @"22:57:16";
+		public static string time = @"20:02:42";
 		public Version()
 		{
 		}
