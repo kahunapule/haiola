3f33dec2ae6763c67cb6a34e6f09bb93445af960
Thomson <Thomson@THOMSONJ-1108.dallas.sil.org>
2012-11-24 15:36:34 -0600

    Merge with default

--- haiola/Version.cs
+++ haiola/Version.cs
@@ -6,9 +6,9 @@ namespace haiola
 	/// </summary>
 	public class Version
 	{
-		public static string date = "2012-11-24";
+		public static string date = "2012-11-23";
 		public static string year = @"2012";
-		public static string time = @"21:26:14";
+		public static string time = @"07:00:48";
 		public Version()
 		{
 		}
