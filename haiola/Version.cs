7d52b062631c797cdc7323cf1e781bdde4b3fb37
Kahunapule <Kahunapule@localhost>
2013-08-14 17:59:24 -1000

    Moved bottom-of-page navigation block to above footnotes in HTML output.

--- haiola/Version.cs
+++ haiola/Version.cs
@@ -6,9 +6,9 @@ namespace haiola
 	/// </summary>
 	public class Version
 	{
-		public static string date = "2013-08-12";
+		public static string date = "2013-08-14";
 		public static string year = @"2013";
-		public static string time = @"22:38:42";
+		public static string time = @"21:17:04";
 		public Version()
 		{
 		}
