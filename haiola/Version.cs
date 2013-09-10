1fcbcb2beeaaa879b99eb8da929e0b9111184167
Kahunapule <Kahunapule@localhost>
2013-09-10 13:56:52 -1000

    Bug fix in Paratext link.

--- haiola/Version.cs
+++ haiola/Version.cs
@@ -6,9 +6,9 @@ namespace haiola
 	/// </summary>
 	public class Version
 	{
-		public static string date = "2013-09-04";
+		public static string date = "2013-09-10";
 		public static string year = @"2013";
-		public static string time = @"01:10:13";
+		public static string time = @"22:13:25";
 		public Version()
 		{
 		}
