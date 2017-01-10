eaae329341a5c78b15dbe819f5e00b5974b1cba0
Kahunapule <Kahunapule@eBible.org>
2017-01-09 15:23:13 -1000

    Cleaning up source files: removing output files that shouldn't be in the source archives and some XSLT files that are no longer in use in the current version of Haiola.

--- JimA/2014-11-29/summary.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call "C:\Program Files\Saxonica\SaxonHE9.4N\bin\transform" -o:listOfBookNames.html BookNames.xml XSLT\step1.xsl
-call "C:\Program Files\Saxonica\SaxonHE9.4N\bin\transform" -o:listOfBookNamesStartingWithDigit.html BookNames.xml XSLT\step2.xsl
-call "C:\Program Files\Saxonica\SaxonHE9.4N\bin\transform" -o:usfx/\convert\usfx.xml.step3.xml c:\d\Kahuna\usfx/\source\usfx.xml XSLT\step3.xsl pathToFiles="C:\d\kahuna\" bookNamesFile="listOfBookNames.html"  bookNamesXml="BookNames.xml"
-call "C:\Program Files\Saxonica\SaxonHE9.4N\bin\transform" -o:usfx/\convert\usfx.xml.step4.xml usfx/\convert\usfx.xml.step3.xml XSLT\step4.xsl
-call "C:\Program Files\Saxonica\SaxonHE9.4N\bin\transform" -o:usfx/\convert\usfx.xml.step5.xml usfx/\convert\usfx.xml.step4.xml XSLT\step5.xsl
-call "C:\Program Files\Saxonica\SaxonHE9.4N\bin\transform" -o:usfx/\convert\usfx.xml.step6.xml usfx/\convert\usfx.xml.step5.xml XSLT\step6.xsl
--- haiola/dist/Haiola/frameFuncs.js
+++
@@ -1,6 +0,0 @@
-﻿// If there is an anchor specified for the frame file, try to apply it to the main page.
-function onLoad() {
-    if (window.location.hash != null && window.location.hash != "") {
-        body.main.document.getElementsByName(window.location.hash.substring(1))[0].scrollIntoView(true);
-    }
-}
