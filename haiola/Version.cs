0fc5e58af13de0f83570104733923c88d3e3da67
Kahunapule <Kahunapule@eBible.org>
2019-07-24 12:27:44 -1000

    Minor bugfix update- bookorder.txt handling

--- haiola/Version.cs
+++ haiola/Version.cs
@@ -6,9 +6,9 @@ namespace haiola
 	/// </summary>
 	public class Version
 	{
-		public static string date = "2019-07-19";
+		public static string date = "2019-07-24";
 		public static string year = @"2019";
-		public static string time = @"04:16:44";
+		public static string time = @"22:08:16";
 		public Version()
 		{
 		}
